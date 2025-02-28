22ffda1b7ca2199c3884a93cacb12c611c06dd54
JN <joyng808@gmail.com>
2025-02-28 16:38:22 +0800

    [JN]- Added Category Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { HomeComponent } from './Home/home/home.component';
+import { HomeModule } from './Home/home/home.module';
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CategoryListComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
