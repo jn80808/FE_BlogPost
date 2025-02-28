cdb4d645794e1419daa90f4b219fc5a864da4d0c
JN <joyng808@gmail.com>
2025-02-28 20:45:47 +0800

    [JN] - Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
+import { HomeComponent } from './Home/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent  // Default route for HomeComponent
+  },
   {
     path:'categories',
     component: CategoryListComponent
