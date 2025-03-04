8848517431b794fcb819dc07bd984456f5b8d7c6
JN <joyng808@gmail.com>
2025-03-05 00:00:53 +0800

    [JN]- Updated

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -1,6 +1,7 @@
 import { afterNextRender, Component } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
@@ -11,6 +12,7 @@ export class AddCategoryComponent {
 
 
   model : AddCategoryRequest;
+  private addCategorySubscribtion?:  Subscription
 
   constructor (private categoryService : CategoryService){
     this.model = {
@@ -23,7 +25,7 @@ export class AddCategoryComponent {
   
 
   onFormSubmit() {
-    this.categoryService.addCategory(this.model)
+    this.addCategorySubscribtion = this.categoryService.addCategory(this.model)
       .subscribe({
         next: (response) => {
           console.log('Category added successfully:', this.model);
