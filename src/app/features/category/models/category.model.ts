8d46fb7e3e84d42245da906bf7b6dccde9915502
JN <joyng808@gmail.com>
2025-03-08 16:18:23 +0800

    [JN]- Update

---
+++ src/app/features/category/models/category.model.ts
@@ -0,0 +1,11 @@
+export interface category {
+
+    Id: string;
+    Name: string;
+    UrlHandle: string;
+    Description : string;
+
+
+}
+
+
--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { category } from '../models/category.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,18 @@ export class CategoryService {
   constructor(private http : HttpClient) { }
 
   addCategory(model: AddCategoryRequest): Observable<void> {
-    return this.http.post<void>('https://localhost:7008/api/BlogCategory', model);
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/BlogCategory`, model);
   }
+
+  getAllCategories(): Observable<category[]>{
+    return this.http.get<category[]>(`${environment.apiBaseUrl}/api/BlogCategory`)
+
+  }
+
+
+
+
+
 }
+
 
