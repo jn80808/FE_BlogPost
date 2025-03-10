8860332b93b25bdd9ea93d2dccceb613850c721d
JN <joyng808@gmail.com>
2025-03-10 21:08:45 +0800

    [JN]- UPDATE

--- src/app/features/category/edit-category/edit-category.component.ts
+++ src/app/features/category/edit-category/edit-category.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-category',
   templateUrl: './edit-category.component.html',
   styleUrls: ['./edit-category.component.css']
 })
-export class EditCategoryComponent {
+export class EditCategoryComponent implements OnInit, OnDestroy{
+
+  id : string | null = null;
+  paramsSubscription?: Subscription;
+  
+
+  constructor (private route : ActivatedRoute){
+
+  }
+ 
+  ngOnInit(): void {
+    this.paramsSubscription =  this.route.paramMap.subscribe({
+      next: (param) =>{
+        this.id =  param.get('id');
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription?.unsubscribe();
+  }
+  
+
 
 }
