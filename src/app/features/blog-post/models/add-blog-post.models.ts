c4e9e13fadabe12c21f774b695bad9eb3ff478e5
JN <joyng808@gmail.com>
2025-03-30 23:18:05 +0800

    [JN]- Added models

---
+++ src/app/features/blog-post/models/add-blog-post.models.ts
@@ -0,0 +1,14 @@
+export interface addBlogPost {
+    //id?: string; // Optional since it  be generated on the backend
+    title: string;
+    shortDescription: string;
+    content: string;
+    featureImageUrl: string;
+    authorName: string;
+    urlHandle: string;
+    publishedDate?: Date | null; // Nullable DateTime
+    isVisible: boolean;
+    categoryId?: string | null; // Nullable Foreign Key
+    //createdAt?: Date; // Auto-generated on the backend
+    isPublished: boolean;
+}
