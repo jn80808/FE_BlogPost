732732a209cecb5253172a4a7e095c970a1f0103
JN <joyng808@gmail.com>
2025-04-13 21:06:21 +0800

    [JN]-Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
 import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
 import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component' 
+import { MarkdownModule } from 'ngx-markdown';
+
+
 
 @NgModule({
   declarations: [
@@ -30,7 +33,8 @@ import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blog
     AppRoutingModule,
     HomeModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MarkdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
