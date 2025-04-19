b29b3925ef585979e02b6c5c500ed030c8b3e681
JN <joyng808@gmail.com>
2025-04-19 20:21:16 +0800

    [JN]- Update added router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AddCategoryComponent } from './features/category/add-category/add-categ
 import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
 import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
 import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component';
+import { EditBlogpostComponent } from './features/blog-post/edit-blogpost/edit-blogpost.component';
 
 const routes: Routes = [
   {
@@ -31,7 +32,12 @@ const routes: Routes = [
   {
     path:'blogposts/add',
     component: AddBlogpostComponent
+  },
+  {
+    path:'blogposts/:id',
+    component: EditBlogpostComponent
   }
+
 
 ];
 
