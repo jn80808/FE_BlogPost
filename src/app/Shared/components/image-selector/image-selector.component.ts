4401620ecbe599504985ee595b6cc5e2573d23fe
JN <joyng808@gmail.com>
2025-04-27 19:49:44 +0800

    [JN]- Added Image selector component

--- src/app/Shared/components/image-selector/image-selector.component.ts
+++ src/app/Shared/components/image-selector/image-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ImageService } from './image.service';
 
 @Component({
   selector: 'app-image-selector',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class ImageSelectorComponent {
 
-}
+
+  file?: File;
+  fileName: string ='';
+  title: string ='';
+  previewUrl: string = '';
+
+  constructor(private imageService: ImageService){
+
+  }
+
+  onFileUploadChange(event : Event): void {
+    const element = event.currentTarget as HTMLInputElement;
+    this.file = element.files?.[0];
+  }
+
+  uploadImage(): void {
+    if (this.file && this.fileName.trim() !== '' && this.title.trim() !== '') {
+      const trimmedFileName = this.fileName.trim();
+      const trimmedTitle = this.title.trim();
+  
+      this.imageService.uploadImage(this.file, trimmedFileName, trimmedTitle)
+        .subscribe({
+          next: (response) => {
+            console.log(response);
+          }
+        });
+    }
+  }
+}  
