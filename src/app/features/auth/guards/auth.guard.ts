722cfb79cde7fe2089fca6dd5574b02f9e8839d6
JN <joyng808@gmail.com>
2025-06-06 00:03:53 +0800

    [JN]- Update

--- src/app/features/auth/guards/auth.guard.ts
+++ src/app/features/auth/guards/auth.guard.ts
@@ -1,5 +1,25 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const cookieService = inject(CookieService);
+  const authService = inject(AuthService)
+
+  //Check for the JWT Token
+  let token = cookieService.get('Authorization');
+
+  if(token){
+
+  } else {
+    // Logout
+    authService.logout();
+
+
+  }
+
+  return ('try')
+
+
 };
