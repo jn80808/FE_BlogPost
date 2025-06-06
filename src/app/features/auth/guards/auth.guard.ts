75616c3319ae0e5c88811a00c45bf08aff827658
JN <joyng808@gmail.com>
2025-06-06 21:19:35 +0800

    [JN]- Update AuthGuard

--- src/app/features/auth/guards/auth.guard.ts
+++ src/app/features/auth/guards/auth.guard.ts
@@ -1,20 +1,49 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthService } from '../services/auth.service';
+import jwt_decode from 'jwt-decode';
+
 
 export const authGuard: CanActivateFn = (route, state) => {
   const cookieService = inject(CookieService);
-  const authService = inject(AuthService)
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  
+
+  interface JwtPayload {
+  sub?: string;
+  exp?: number;
+  // Add any other claims you expect in the token
+}
 
   //Check for the JWT Token
   let token = cookieService.get('Authorization');
 
   if(token){
+      token = token.replace('Bearer','');
+      const decodedtoken : any = jwt_decode(token);
+
+      //Check if token has expired 
+      const expirationDate = decodedtoken.exp*1000;
+      const currentTime = new Date().getTime();
+
+      //compare if the token is not expire 
+      if (expirationDate < currentTime){
+      // Logout if token is not valid 
+      authService.logout();
+      return router.createUrlTree(['/login'],{queryParams: {returnUrl: state.url}})
+      } else {
+        //Token is still valid 
+
+        
+      }
 
   } else {
     // Logout
     authService.logout();
+    return router.createUrlTree(['/login'],{queryParams: {returnUrl: state.url}})
+    
 
 
   }
